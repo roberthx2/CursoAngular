2a790ee117ae0d5d00367951e8283e8178216e63
Roberth Riera <riera_1992@hotmail.com>
2018-09-01 10:13:39 -0430

    Fundamentos de Angular

---
+++ notas.php
@@ -0,0 +1,63 @@
+
+1. Instalar Angular CLI
+
+    npm install -g @angular/cli
+
+2. Vefiricar version de angular/cli
+
+    ng --version
+
+3. Crear proyecto angular
+
+    ng new /path
+
+4. Opciones al crear un proyecto
+
+	ng new /path
+
+		--routing --> Genera un modulo exclusivo para las rutas
+		--prefix pre --> Componentes con prefijos
+		--dry-run --> Debud
+		 --style scss --> Tipos de estilos para el proyecto
+		 --skip-install --> Controla las dependencias
+
+5. Estructura de directorios
+
+	- angular.json: Configuracion del proyecto
+	- .editorconfig: Configuracion del editor
+
+6. Instalar dependencias
+
+	npm install
+
+7. Preview de la app 
+
+	ng serve
+
+8. General build
+
+	ng build --
+	
+9. Probar app con http-server  
+
+	http-serve path
+
+10. Realizar unit-test
+
+	ng test
+
+11. Test e2e
+
+	ng e2e
+
+12. Generar compoonente
+
+	ng generate component login
+
+13. Generar class
+
+	ng generate class usuario
+
+14. Generar modulo
+
+	ng generate module login
